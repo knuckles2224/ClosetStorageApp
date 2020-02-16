6ecba7e6feca59483e130b17b9c6f29969a37aa2
knuckles2224 <54727254+knuckles2224@users.noreply.github.com>
2020-02-16 18:16:57 -0500

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ClosetStorageApp
+//
+//  Created by Anthony Onn on 2/16/20.
+//  Copyright © 2020 Anthony Onn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
